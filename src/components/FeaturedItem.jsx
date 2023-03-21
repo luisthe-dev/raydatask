2dad22d877bb46328ca8dbb3437624683c6d9acb
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2023-03-21 15:00:17 +0100

    implement api fetch for featured items

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 
-const FeaturedItem = () => {
+const FeaturedItem = ({ item }) => {
   return (
     <div className="featuredItem">
-      <img src="" alt=".." />
+      <img src={item.image} alt={item.title} />
       <div className="featuredBuyer">
         <p> KO </p>
         <h4>
-          Koray Okumus <span>(Highest Bidder)</span>
+          {item.name} <span>(Highest Bidder)</span>
         </h4>
       </div>
       <div className="featuredItemDetails">
-        <h5>Apple MacBook Air 13" M1 Chip 8GB 256GB 2020 Model - Rose Gold</h5>
+        <h5>{item.title}</h5>
         <h6>
-          Current Bid: <p>₦795,000</p>
+          Current Bid: <p>{item.bid}</p>
         </h6>
       </div>
       <div className="featuredButton">
--- src/components/FeaturedItems.jsx
+++ src/components/FeaturedItems.jsx
@@ -1,8 +1,25 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import FeaturedItem from "./FeaturedItem";
 
 const FeaturedItems = () => {
-  const [featuredItems, setFeaturedItems] = useState(["", "", "", "", "", ""]);
+  const [featuredItems, setFeaturedItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchAllItems = async () => {
+    const fetchedItems = await axios.get(
+      "https://run.mocky.io/v3/7f02819f-8254-410a-b8af-ab98572bd26b"
+    );
+
+    if (fetchedItems.status !== 200) {
+      setFeaturedItems([]);
+      return;
+    }
+
+    setFeaturedItems(fetchedItems.data.data);
+  };
+
+  useEffect(() => fetchAllItems, []);
 
   return (
     <div className="auctionCard">
@@ -11,8 +28,8 @@ const FeaturedItems = () => {
         <button> View Auction </button>
       </div>
       <div className="featuredItems">
-        {featuredItems.map((featuredItem) => (
-          <FeaturedItem item={featuredItem} />
+        {featuredItems.map((featuredItem, key) => (
+          <FeaturedItem item={featuredItem} key={key} />
         ))}
       </div>
     </div>
